f90d847a1a246d8c081cf4d2bb3a5f9d6ff679c6
Alejandro <da.hurtado09@ciaf.edu.co>
2025-11-26 18:21:27 -0500

    Detalles minimos del front

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/auth";
+const API_URL = "http://localhost:3001/api/v1/auth";
 
 export const loginRequest = async (email, password) => {
   const response = await axios.post(`${API_URL}/login`, {
